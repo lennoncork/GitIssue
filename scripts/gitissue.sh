1c654d1fb1497b3faf6271d74cbf0de1f63ce9fa
Lennon Cork <lennon.cork@gmail.com>
2020-03-18 12:01:32 -0700

    Update to support GitIssue as a dotnet tool

--- scripts/gitissue.sh
+++ scripts/gitissue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPTPATH=${0%/*}
-"$SCRIPTPATH/../src/output/GitIssue.Util/bin/Release/netcoreapp3.1/win10-x64/publish/GitIssue.Util.exe" "$@"
+"$SCRIPTPATH/../src/output/GitIssue.Tool/bin/Release/netcoreapp3.1/win10-x64/publish/GitIssue.Tool.exe" "$@"
